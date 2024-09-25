2f1799b7e308bb76636763fd7ec213cea06b50fd
Oliver Mravec <mravecoliver@gmail.com>
2024-09-25 09:15:54 +0200

    add vstup- vystupand vypocty to 01/2

---
+++ 01-uvod/02-vstup-vystup.py
@@ -0,0 +1,7 @@
+a = 100 # = je príkaz priradenia, tzn. že v premennej a bude hodnota 10
+print(a) # print je príkaz prístupu, vypíše do terminálu obsah premennej a
+b = 200
+print("Vpremennej b je hodnota:") # ako chceme vypísať text, davame uvodzovky, ak chceme vypísať obsah premennej, dávame len názov premennej(bez úvodzoviek)
+print(b)
+print("V premennej b je hodnota:", b) # ak kombinujeme pri výpise reťazec s premenou, oddeľujeme čiarkami
+print("V premennej b je hodnota:", a, "V premennej b je hodnota:", b) 
